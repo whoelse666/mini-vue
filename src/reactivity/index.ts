989b14388c97c93cd3f35c7993b2991162e61eaa
whoelse <prwabc@126.com>
2024-02-23 17:51:52 +0800

    init jest

---
+++ src/reactivity/index.ts
@@ -0,0 +1,4 @@
+export function add(a, b) {
+  console.log('a,b', a, b);
+  return a+b
+}
---
+++ src/reactivity/tests/index.spec.ts
@@ -0,0 +1,7 @@
+
+import { add } from '../index.ts';
+it('init', () => {
+  console.log('test');
+  expect(true).toBe(true);
+  expect(add(1,1)).toBe(2);
+});
