b1e41d83f4e9123525f4539e39a4b4e4fddf5dd7
whoelse <prwabc@126.com>
2024-02-23 18:19:48 +0800

    add effect.spec.ts

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,3 +1,10 @@
-it('init', () => {
-  expect(true).toBe(true);
+// import { reactive } from '../src/reactive.ts';
+// import { effect, stop } from '../src/effect.ts';
+import { add } from '../index.ts';
+
+describe('effect', () => {
+  it('should run the passed function once (wrapped by a effect)', () => {
+    expect(true).toBe(true);
+    expect(add(1, 1)).toBe(2);
+  });
 });
