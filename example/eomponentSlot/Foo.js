e63d67439b015bda78c7382bbb10e4ad2f6eaabb
whoelse <prwabc@126.com>
2024-03-12 16:25:41 +0800

    feat:实现 Fragment 和 Text 类型节点

--- example/eomponentSlot/Foo.js
+++ example/eomponentSlot/Foo.js
@@ -8,8 +8,10 @@ export const Foo = {
     // /foo .vnode.children
     // const renderSlots = array.isArray(this.$slots) ? h("div", { class: "foo" }, [foo, ...this.$slots]) : h("div", { class: "foo" }, [foo, this.$slots]);
     // const renderSlots = h("div", { class: "foo" }, [foo, this.$slots]);
+
+    // return h("div", { class: "foo" }, [renderSlots(this.$slots, "footer", { name: "哈利波特帅" }), foo, renderSlots(this.$slots, "header", { num: 99 })]);
     return h("div", { class: "foo" }, [renderSlots(this.$slots, "footer", { name: "哈利波特帅" }), foo, renderSlots(this.$slots, "header", { num: 99 })]);
-    // return h("div", { class: "foo" }, [renderSlots(this.$slots, "header"), foo, renderSlots(this.$slots, "footer")]);
+
   }
 };
 
